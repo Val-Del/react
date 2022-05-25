47a397c5e9d0bcdd8bc050cd1cb97aeb3a0a2bdb
Valentin <valentinlaptop>
2022-05-25 10:20:57 +0200

    nav

--- main.jsx
+++ main.jsx
@@ -1,11 +1,24 @@
-// let welcome = React.createElement('h1', {}, 'cc le monde')
-// ReactDOM.render(welcome, document.body)
+// function Welcome({name}){
+//     return <h1>Salut {name}</h1>
+// }
 
-function Welcome({name}){
-    return <h1>Salut {name}</h1>
+// ReactDOM.render(<Welcome name="valentin"/>, document.body)
+function NavButton({name}){
+return <button>{name}</button>
 }
-
-ReactDOM.render(<Welcome name="valentin"/>, document.body)
-
-// const domContainer = document.querySelector('#like_button_container');
-// ReactDOM.render(e(LikeButton), domContainer);
+function Nav() {
+    return <nav>
+        <NavButton name="accueil"/>
+        <NavButton name="compétences"/>
+        <NavButton name="contact"/>
+        {/* <div className="center">
+            <ul className="flex">
+                <li><navButton name="accueil"></navButton></li>
+                <li><button>{children}</button></li>
+                <li><button>{children}</button></li>
+            </ul>
+        </div> */}
+    </nav>
+}
+ReactDOM.render(<Nav></Nav>, document.body.querySelector('header'))
+// ReactDOM.render(<NavButton name="accueil"/>, document.body.querySelector('header'))
